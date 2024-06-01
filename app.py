59d3047b6942ad95cfc6d6d5923f6e9820430fc1
Levi Evans <levi.samuel.evans@cern.ch>
2024-06-01 03:55:37 +0100

    final formatting

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from joblib import load
 import pandas as pd
 
 app = Flask(__name__)
-model = load('models/best_model.joblib')
+model = load("models/best_model.joblib")
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
     df = pd.DataFrame(data)
@@ -17,5 +19,6 @@ def predict():
     predictions = [0 if x == 1 else 1 for x in predictions]
     return jsonify(predictions)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
